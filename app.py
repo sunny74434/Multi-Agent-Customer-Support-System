4f23f2a1f6e9b7f164c987c53fd396518b0d4bb4
sunny sahu <sunnysahu74434@gmail.com>
2026-06-24 20:30:24 +0530

    Update app.py for Hugging Face deployment

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from ui import gui
 
 if __name__ == "__main__":
-    gui.launch(debug=True)    
+    gui.launch(
+        server_name="0.0.0.0",
+        server_port=7860,
+        debug=True,
+    )
